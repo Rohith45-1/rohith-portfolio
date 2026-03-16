f0db53ec0ea1761c57d6032512ef5d9804a549da
Rohith45-1 <21273ec052@gmail.com>
2026-03-16 21:41:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
-function toggleMode(){
+// Scene
+const scene = new THREE.Scene();
 
-document.body.classList.toggle("light")
+// Camera
+const camera = new THREE.PerspectiveCamera(
+75,
+window.innerWidth / window.innerHeight,
+0.1,
+1000
+);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+// Create stars
+function addStar() {
+
+const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+
+const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+
+const star = new THREE.Mesh(geometry, material);
+
+const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+star.position.set(x, y, z);
+
+scene.add(star);
 
 }
 
-function chatbot(){
+// Add many stars
+Array(200).fill().forEach(addStar);
 
-alert("Hello! I am Rohith's AI assistant. How can I help you?")
+// Animation
+function animate() {
+
+requestAnimationFrame(animate);
+
+renderer.render(scene, camera);
 
 }
+
+animate();
